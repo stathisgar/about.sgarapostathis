608b382f397091210178f2808104402413306612
stathisgar <sgarapostathis@alittlemap.gr>
2026-06-21 19:43:32 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 var  header = document.getElementById("header");
-var newHeader = ["New article #1: Introducing Deucalion and Pyrrha v1.0: image datasets for DM of floods","New article #2: Exploiting Multiple Social Media Sources and Multiple Modalities for Severe Weather Management: The Case Study of the Medicane Ianos.", "Dr. Stathis G. Arapostathis"];
+var newHeader = [];
 var i = 0;
 
 header.innerHTML = "Dr. Stathis G. Arapostathis";
